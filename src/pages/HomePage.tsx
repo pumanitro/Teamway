3e4bc2d9a9e337b73add2afe85746fb49ba0283b
patrykjanikabilitie <patryk.janik@abilitie.com>
2023-03-11 20:57:26 +0100

    Added AI generated graphics for questions.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Card, CardActions, CardContent, Typography } from "@mui/material";
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
 import { AppWrapper } from "../components/AppWrapper";
 import { ROUTING_KEYS } from "../utils/routingKeys";
 import { Link } from "react-router-dom";
@@ -9,20 +9,18 @@ import IntroversAndExtrovert from "../assets/introvert_and_extrovert.png";
 export const HomePage = () => {
   const { setQuestions } = useQuestionsContext();
   return (
-    <AppWrapper
-      wrapperStyles={{
-        backgroundImage: `url(${IntroversAndExtrovert})`,
-        backgroundRepeat: `no-repeat, repeat`,
-        backgroundPosition: `left`,
-      }}
-    >
-      <Card sx={{ p: 2 }}>
+    <AppWrapper>
+      <Card sx={{ p: 2, width: "70%", m: 4, minHeight: "60vh" }}>
+        <CardMedia component="img" sx={{ height: "70vh" }} image={IntroversAndExtrovert} />
         <CardContent>
-          <Typography variant="h5">Welcome to personality test!</Typography>
+          <Typography variant="h5" sx={{ mb: 1 }}>
+            Welcome to personality test!
+          </Typography>
           <Typography>
             It takes 5 different questions, maps them into a score and produces a personality trait of either Introvert
             or Extrovert.
           </Typography>
+          <Typography>Going through test you will see beautiful images created by AI. Enjoy!</Typography>
         </CardContent>
         <CardActions sx={{ justifyContent: "flex-end" }}>
           <Link to={ROUTING_KEYS.QUESTION(0)} style={{ textDecoration: "none" }} onClick={() => setQuestions([])}>
--- src/pages/QuestionPage.tsx
+++ src/pages/QuestionPage.tsx
@@ -8,16 +8,26 @@ import {
   Card,
   CardActions,
   CardContent,
+  CardMedia,
   CircularProgress,
   FormControl,
   FormControlLabel,
   Radio,
   RadioGroup,
+  Stack,
   Typography,
 } from "@mui/material";
 import { ROUTING_KEYS } from "../utils/routingKeys";
 import { useQuestionsContext } from "../contexts/QuestionsContext";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+
+import Question0 from "../assets/question_0.png";
+import Question1 from "../assets/question_1.png";
+import Question2 from "../assets/question_2.png";
+import Question3 from "../assets/question_3.png";
+import Question4 from "../assets/question_4.png";
+
+const questionImages = [Question0, Question1, Question2, Question3, Question4];
 
 export const QuestionPage = () => {
   const { id } = useParams();
@@ -79,27 +89,30 @@ export const QuestionPage = () => {
 
   return (
     <AppWrapper>
-      <Card sx={{ p: 2, width: "90%", m: 4, minHeight: "80vh" }}>
-        <CardContent style={{ minHeight: `calc(80vh - 100px)` }}>
-          {isTheFirstQuestion ? null : (
-            <Link to={ROUTING_KEYS.QUESTION(Number(id) - 1)} style={{ textDecoration: "none" }}>
-              <Button startIcon={<ArrowBackIcon />}>Previous</Button>
-            </Link>
-          )}
-          <Typography variant="h5">{question}</Typography>
-          <FormControl>
-            <RadioGroup
-              aria-labelledby="questions-radio-group"
-              name="radio-buttons-group"
-              id={`question-id`}
-              value={questions.find((q) => q.id === id)?.answer?.id || null}
-              onChange={onAnswerChange}
-            >
-              {answers.map((answer, index) => (
-                <FormControlLabel key={index} value={index} control={<Radio />} label={answer.answerText} />
-              ))}
-            </RadioGroup>
-          </FormControl>
+      <Card sx={{ p: 2, width: "70%", m: 4, minHeight: "60vh" }}>
+        {isTheFirstQuestion ? null : (
+          <Link to={ROUTING_KEYS.QUESTION(Number(id) - 1)} style={{ textDecoration: "none" }}>
+            <Button startIcon={<ArrowBackIcon />}>Previous</Button>
+          </Link>
+        )}
+        <CardMedia component="img" sx={{ height: "70vh" }} image={questionImages[Number(id)]} />
+        <CardContent style={{ minHeight: `calc(10vh - 100px)` }}>
+          <Stack>
+            <Typography variant="h5">{question}</Typography>
+            <FormControl>
+              <RadioGroup
+                aria-labelledby="questions-radio-group"
+                name="radio-buttons-group"
+                id={`question-id`}
+                value={questions.find((q) => q.id === id)?.answer?.id || null}
+                onChange={onAnswerChange}
+              >
+                {answers.map((answer, index) => (
+                  <FormControlLabel key={index} value={index} control={<Radio />} label={answer.answerText} />
+                ))}
+              </RadioGroup>
+            </FormControl>
+          </Stack>
         </CardContent>
         <CardActions sx={{ justifyContent: "flex-end" }}>
           {isTheLastQuestion ? (
